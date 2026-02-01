8b89e7bf2f41b2e289306d1285bebd78de26c033
Monodess <margfritadoni@gmail.com>
2026-02-01 23:09:37 +0200

    Registration added

---
+++ AspUrlShortnerer/Application/RegistrationApplcation.cs
@@ -0,0 +1,13 @@
+﻿using AspUrlShortnerer.Domain.Services;
+
+namespace AspUrlShortnerer.Application
+{
+    public class RegistrationApplcation
+    {
+        public RegistrationApplcation() { }
+        public UserLogin CreateUser()
+        {
+            return new UserLogin();
+        }
+    }
+}
---
+++ AspUrlShortnerer/Infrastructure/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using AspUrlShortnerer.Domain.entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace AspUrlShortnerer.Infrastructure
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<ShortenUrl> ShortenUrls => Set<ShortenUrl>();
+        public DbSet<UserLogin> UserLogins => Set<UserLogin>(); 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ShortenUrl>()
+                .Property(x => x.Id)
+                .ValueGeneratedOnAdd();
+            modelBuilder.Entity<ShortenUrl>()
+                .Property(y => y.Code)
+                .HasColumnType("VARCHAR(6)");
+            modelBuilder.Entity<ShortenUrl>()
+                .Property(s => s.ShortUrl)
+                .HasColumnType("VARCHAR(29)"); //https://localhost:5020/unique 
+            modelBuilder.Entity<ShortenUrl>()
+                .Property(l => l.LongUrl)
+                .HasColumnType("VARCHAR(2048)");
+        }
+    }
+}
